af0d0b33d834cf4cbb56282bf5d065a4858e0b44
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-12-21 23:38:04 +0530

    Initial Commit

---
+++ src/main/java/com/aspect/oriented/AopApiApplication.java
@@ -0,0 +1,13 @@
+package com.aspect.oriented;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopApiApplication.class, args);
+	}
+
+}
