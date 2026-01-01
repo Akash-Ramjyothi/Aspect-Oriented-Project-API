b367e7f7ddcbe4b39de4784aac1bb28f06aa0563
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-01-01 22:44:13 +0530

    Declared Pointcut() expression

--- src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
+++ src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
@@ -2,16 +2,22 @@ package com.aspect.oriented.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
+    @Pointcut("execution(* com.aspect.oriented.dao.*.*(..))")
+    private void forDaoPackage() {
+
+    }
+
     //    @Before("execution(public void updateAccount())")
 //    @Before("execution(public void add*())")
 //    @Before("execution(* add*())")
-    @Before("execution(* com.aspect.oriented.dao.*.*(..))")
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
     }
