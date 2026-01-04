4e7143d28c7c894e554b00ebf060400b0954f23a
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-01-04 23:11:37 +0530

    Combined Pointcut expressions

--- src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
+++ src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
@@ -11,19 +11,30 @@ public class LoggingAspect {
 
     @Pointcut("execution(* com.aspect.oriented.dao.*.*(..))")
     private void forDaoPackage() {
+    }
 
+    @Pointcut("execution(* com.aspect.oriented.dao.*.get*(..))")
+    private void getter() {
+    }
+
+    @Pointcut("execution(* com.aspect.oriented.dao.*.set*(..))")
+    private void setter() {
+    }
+
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {
     }
 
     //    @Before("execution(public void updateAccount())")
 //    @Before("execution(public void add*())")
 //    @Before("execution(* add*())")
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
     }
 
-    @Before("forDaoPackage()")
-    public void performingApiAnalytics(){
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performingApiAnalytics() {
         System.out.println("\n====>>> Performing API analytics");
     }
 }
