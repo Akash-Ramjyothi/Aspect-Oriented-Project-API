413c291b3f967b7b44aab2dbec0261c403c2200f
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-01-05 23:30:09 +0530

    Added JoinPoints to method arguments

--- src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
+++ src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
@@ -1,10 +1,13 @@
 package com.aspect.oriented.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.lang.classfile.MethodSignature;
 
 @Aspect
 @Component
@@ -15,7 +18,11 @@ public class LoggingAspect {
 //    @Before("execution(public void add*())")
 //    @Before("execution(* add*())")
     @Before("com.aspect.oriented.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
+
+        org.aspectj.lang.reflect.MethodSignature methodSignature = (org.aspectj.lang.reflect.MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
     }
 }
