bd01667d04c6d4e9e421804f1033e44c5cd5161b
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-01-07 23:45:51 +0530

    Added @AfterReturning() advice

--- src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
+++ src/main/java/com/aspect/oriented/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.aspect.oriented.aspect;
 
 import com.aspect.oriented.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,11 +10,24 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.lang.classfile.MethodSignature;
+import java.util.List;
 
 @Aspect
 @Component
 @Order(2)
 public class LoggingAspect {
+
+    @AfterReturning(
+            pointcut = "execution(* com.aspect.oriented.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdivce(JoinPoint theJoinPoint, List<Account> result) {
+
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
+
+        System.out.println("\n=====>>> Result is: " + result);
+    }
 
     //    @Before("execution(public void updateAccount())")
 //    @Before("execution(public void add*())")
@@ -27,10 +41,10 @@ public class LoggingAspect {
 
         Object[] args = theJoinPoint.getArgs();
 
-        for(Object tempArg: args){
+        for (Object tempArg : args) {
             System.out.println("tempArg: " + tempArg);
 
-            if(tempArg instanceof Account){
+            if (tempArg instanceof Account) {
                 Account theAccount = (Account) tempArg;
 
                 System.out.println("Account Name: " + theAccount.getName());
