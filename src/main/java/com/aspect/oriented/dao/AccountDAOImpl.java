b85be86bf5f17882981b8c4d4aa5ec6689b0c631
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-03-20 23:53:48 +0530

    feat: enhance AccountDAOImpl with in-memory store and extended CRUD support

--- src/main/java/com/aspect/oriented/dao/AccountDAOImpl.java
+++ src/main/java/com/aspect/oriented/dao/AccountDAOImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class AccountDAOImpl implements AccountDAO {
@@ -12,54 +13,74 @@ public class AccountDAOImpl implements AccountDAO {
     private String name;
     private String serviceCode;
 
+    // Simulated in-memory storage
+    private final List<Account> accountStore = new ArrayList<>();
+
     @Override
     public List<Account> findAccounts() {
-//        List<Account> myAccounts = new ArrayList<>();
-//
-//        Account temp1=new Account("John","Silver");
-//        Account temp2=new Account("Madhu","Platinum");
-//        Account temp3=new Account("Luca","Gold");
-//
-//        myAccounts.add(temp1);
-//        myAccounts.add(temp2);
-//        myAccounts.add(temp3);
-//
-//        return myAccounts;
-
         return findAccounts(false);
     }
 
     @Override
     public List<Account> findAccounts(boolean tripWire) {
 
-        if (tripWire == true) {
-            throw new RuntimeException("No soup for you !!!");
+        if (tripWire) {
+            throw new RuntimeException("Simulated exception: unable to fetch accounts");
         }
 
-        List<Account> myAccounts = new ArrayList<>();
-
-        Account temp1 = new Account("John", "Silver");
-        Account temp2 = new Account("Madhu", "Platinum");
-        Account temp3 = new Account("Luca", "Gold");
-
-        myAccounts.add(temp1);
-        myAccounts.add(temp2);
-        myAccounts.add(temp3);
-
-        return myAccounts;
+        // Return copy to avoid external modification
+        return new ArrayList<>(accountStore);
     }
 
     @Override
     public void addAccount(Account theAccount, boolean vipFlag) {
-        System.out.println(getClass() + " : DOING MY DB WORK : ADDING AN ACCOUNT");
+
+        System.out.println(getClass() + " : Adding account -> " + theAccount);
+
+        if (vipFlag) {
+            System.out.println(getClass() + " : VIP account detected");
+        }
+
+        accountStore.add(theAccount);
     }
 
     @Override
     public boolean doWork() {
-        System.out.println(getClass() + " : doWork()");
+        System.out.println(getClass() + " : Performing work...");
+
+        // Simulate some logic
+        return true;
+    }
+
+    // Additional CRUD-like methods
+
+    public Optional<Account> findAccountByName(String accountName) {
+        return accountStore.stream()
+                .filter(acc -> acc.getName().equalsIgnoreCase(accountName))
+                .findFirst();
+    }
+
+    public boolean updateAccount(String accountName, String newLevel) {
+        Optional<Account> accountOpt = findAccountByName(accountName);
+
+        if (accountOpt.isPresent()) {
+            accountOpt.get().setLevel(newLevel);
+            return true;
+        }
 
         return false;
     }
+
+    public boolean deleteAccount(String accountName) {
+        return accountStore.removeIf(acc ->
+                acc.getName().equalsIgnoreCase(accountName));
+    }
+
+    public void clearAccounts() {
+        accountStore.clear();
+    }
+
+    // Getters & Setters with logging (useful for AOP demos)
 
     public String getName() {
         System.out.println(getClass() + " : in getName()");
@@ -67,7 +88,7 @@ public class AccountDAOImpl implements AccountDAO {
     }
 
     public void setName(String name) {
-        System.out.println(getClass() + " : in serName()");
+        System.out.println(getClass() + " : in setName()");
         this.name = name;
     }
 
